a5eb90e922e9bebd0b1e9c6e98a80edc893e57b4
12anuja <anujapriya96@gmail.com>
2019-07-16 19:56:59 +0530

    Task 1

---
+++ src/main/java/com/stackroute/ConfigClass.java
@@ -0,0 +1,30 @@
+package com.stackroute;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com.stackroute")
+@EnableWebMvc
+public class ConfigClass
+{
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
+
---
+++ src/main/webapp/WEB-INF/login.jsp
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+<title>Home</title>
+</head>
+<body>
+	<form action="hello" method="post">
+		<input type="text" name="userName"><br>
+		<input type="password" name="password"><br>
+		 <input type="submit" value="Login">
+	</form>
+</body>
+</html>
